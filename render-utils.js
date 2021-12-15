c7940215c43e21f1b82aba19077c362971c9e4cf
CarolynnFleming <cfcatpink@gmail.com>
2021-12-15 08:49:57 -0800

    g

---
+++ render-utils.js
@@ -0,0 +1,5 @@
+export function renderBbabyList(bbaby){
+    const bbabyEl = document.createElement('a');
+    const typeEl = document.createElement('p');
+    const waterEl = document.createElement('p');
+}

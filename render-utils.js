979e247f367b38a3976e280bce691416710947fc
CarolynnFleming <cfcatpink@gmail.com>
2021-12-17 15:30:12 -0800

    g

--- render-utils.js
+++ render-utils.js
@@ -1,5 +1,18 @@
 export function renderBbabyList(bbaby){
     const bbabyEl = document.createElement('a');
     const typeEl = document.createElement('p');
-    const waterEl = document.createElement('p');
+    const nameEl = document.createElement('p');
+    const yearEl = document.createElement('p');
+    const fluffEl = document.createElement('p');
+
+    bbabyEl.classList.add('bbaby');
+    bbabyEl.href = `./bbaby/?id=${bbaby.id}`;
+    typeEl.textContent = bbaby.type;
+    nameEl.textContent = bbaby.name;
+    yearEl.textContent = bbaby.year;
+    fluffEl.textContent = bbaby.fluff;
+
+    bbabyEl.append(typeEl, nameEl, yearEl, fluffEl);
+
+    return bbabyEl; 
 }

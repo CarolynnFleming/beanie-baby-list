0228abe8713efa8c02beae656dbfd71811281047
CarolynnFleming <cfcatpink@gmail.com>
2022-01-07 12:38:37 -0800

    more

--- detail/detail.js
+++ detail/detail.js
@@ -1,2 +1,15 @@
 import { getBbaby } from '../fetch-utils.js';
 import { renderBbabyDetail } from '../render-utils.js';
+
+const boxDetail = document.getElementById('beanie-baby-detail-box');
+
+window.addEventListener('load', async() => {
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get('id');
+
+    const bbaby = await getBbaby(id);
+
+    const bbabyDetailEl = renderBbabyDetail(bbaby);
+
+    boxDetail.append(bbabyDetailEl);
+});
